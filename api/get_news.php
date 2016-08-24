9a5b0758da1b85ae16acc4fab17dac16fd397789
Chia-Ting <din1030@gmail.com>
2016-08-25 01:51:05 +0800

    add news api

---
+++ api/get_news.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+
+include 'db_setting.php';
+
+$news_id = $_GET['news_id'];
+
+$sql_string = 'SELECT * FROM `news`';
+if (!empty($news_id)) {
+    $sql_string .= " WHERE `id`=$news_id LIMIT 1";
+}
+
+$sql = $mysqli->query($sql_string);
+if ($sql->num_rows > 0) {
+    while ($r = mysqli_fetch_assoc($sql)) {
+        $output[] = $r;
+    }
+    echo json_encode(array('status' => true, 'result' => $output));
+} else {
+    $output = array('status' => false, 'message' => '請重新操作！');
+    echo json_encode($output);
+}
