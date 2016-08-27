b0e8d49372f19e323c38b5e669a47affb5c755b3
Chia-Ting <din1030@gmail.com>
2016-08-27 21:06:39 +0800

    api bug fix

--- api/remove_fav.php
+++ api/remove_fav.php
@@ -8,7 +8,7 @@ include 'db_setting.php';
 $fav_id = $_POST['fav_id'];
 
 // 判斷是否帳號已經存在
-$sql_string = "DELETE FROM `favorite` WHERE `fav_id` = $fav_id";
+$sql_string = "DELETE FROM `favorite` WHERE `id` = $fav_id";
 $sql = $mysqli->query($sql_string);
 if ($mysqli->affected_rows > 0) {
     $output = array('status' => true, 'message' => '已刪除收藏項目！');
--- www/js/favorite.js
+++ www/js/favorite.js
@@ -26,22 +26,23 @@ $(document).on('pagebeforecreate', '#favorite', function() {
 				var fav_div = $('<div></div>').addClass('detail_block msg_block').attr('data-type', obj.type).attr('data-user-id', user_id)
 					.append('<div class="ui-bar ui-bar-a"><sapn>' + type_word + '</sapn><button class="ui-btn ui-corner-all ui-btn-inline ui-mini ui-btn-right ui-icon-delete ui-btn-icon-notext del-fav-btn" type="button" name="button" data-fav-id="' + obj.fav_id + '" data-shadow="false">x</button></div><a href="' + link + '" data-ajax="false"><div class="ui-body ui-body-a"><div class="avatar msg-avatar float-left"><img src="http://52.69.53.255/KCCordova/www/img/' + obj.pic1 + '" style="width: 70px; height: 70px; border-radius: 50%;" alt=""></div><strong>' + name + '</strong><br>' + obj.country + '｜' + obj.area + '</div></a>');
 				$(fav_div).appendTo($('[data-role="main"]'));
-				$('.del-fav-btn').click(function(event) {
-					var fav_id = $(this).jqmData('fav-id');
-					$.ajax({
-						url: 'http://52.69.53.255/KCCordova/api/remove_fav.php',
-						type: 'POST',
-						dataType: 'json',
-						data: {
-							fav_id: fav_id
-						}
-					}).done(function() {
-						$(this).parents('.detail_block').remove();
-					}).fail(function() {
-						alert('請確認您的網路連線狀態！');
-					});
 
+			});
+			$('.del-fav-btn').click(function(event) {
+				var fav_id = $(this).jqmData('fav-id');
+				$.ajax({
+					url: 'http://52.69.53.255/KCCordova/api/remove_fav.php',
+					type: 'POST',
+					dataType: 'json',
+					data: {
+						fav_id: fav_id
+					}
+				}).done(function() {
+					$(this).parents('.detail_block').remove();
+				}).fail(function() {
+					alert('請確認您的網路連線狀態！');
 				});
+
 			});
 		} else {
 			$('<div>' + data.message + '</div>').appendTo($('[data-role="main"]'));
