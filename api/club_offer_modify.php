4c12566bb6472aee8c3e8c1a1c8653995b76132f
Chia-Ting <din1030@gmail.com>
2016-08-28 15:39:29 +0800

    bug fix

--- api/club_offer_modify.php
+++ api/club_offer_modify.php
@@ -17,7 +17,7 @@ $welfare = $_POST['welfare'];
 $sql_string = "SELECT * FROM `club_offer` WHERE `admin_id` = '$admin_id' LIMIT 1";
 $sql = $mysqli->query($sql_string);
 if ($sql->num_rows > 0) {
-    $update_string = "UPDATE `club_offer` SET `interviewer`='$interviewer',`tel`='$tel',`line`='$line',`offer_content`=''$offer_content',`welfare`='$welfare' WHERE `admin_id` = '$admin_id'";
+    $update_string = "UPDATE `club_offer` SET `interviewer`='$interviewer',`tel`='$tel',`line`='$line',`offer_content`='$offer_content',`welfare`='$welfare' WHERE `admin_id` = '$admin_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
         $output = array('status' => true, 'message' => '資料已修改！');
