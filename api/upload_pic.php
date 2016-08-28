92ff468ddb17aa63c8252bf5d93140587dbb9e57
Chia-Ting <din1030@gmail.com>
2016-08-28 12:01:59 +0800

    fix api

--- api/upload_pic.php
+++ api/upload_pic.php
@@ -15,12 +15,11 @@ if ($type == 'seeker') {
     $table == '`club_info`';
 }
 
-$output = array();
 $target_dir = '../www/img/';
 
 $new_filename1 = $u_id.'_'.basename($_FILES['pic_1']['name']);
 $target_file1 = $target_dir.$new_filename1;
-if (move_uploaded_file($_FILES['pic_1']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['pic_1']['tmp_name'], $target_file1)) {
     $update_string = "UPDATE $table SET `pic1`='$new_filename1' WHERE `u_id` = '$user_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
@@ -34,7 +33,7 @@ if (move_uploaded_file($_FILES['pic_1']['tmp_name'], $target_file)) {
 
 $new_filename2 = $u_id.'_'.basename($_FILES['pic_2']['name']);
 $target_file2 = $target_dir.$new_filename2;
-if (move_uploaded_file($_FILES['pic_2']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['pic_2']['tmp_name'], $target_file2)) {
     $update_string = "UPDATE $table SET `pic2`='$new_filename2' WHERE `u_id` = '$user_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
@@ -48,7 +47,7 @@ if (move_uploaded_file($_FILES['pic_2']['tmp_name'], $target_file)) {
 
 $new_filename3 = $u_id.'_'.basename($_FILES['pic_3']['name']);
 $target_file3 = $target_dir.$new_filename3;
-if (move_uploaded_file($_FILES['pic_3']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['pic_3']['tmp_name'], $target_file3)) {
     $update_string = "UPDATE $table SET `pic3`='$new_filename3' WHERE `u_id` = '$user_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
@@ -62,7 +61,7 @@ if (move_uploaded_file($_FILES['pic_3']['tmp_name'], $target_file)) {
 
 $new_filename4 = $u_id.'_'.basename($_FILES['pic_4']['name']);
 $target_file4 = $target_dir.$new_filename4;
-if (move_uploaded_file($_FILES['pic_4']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['pic_4']['tmp_name'], $target_file4)) {
     $update_string = "UPDATE $table SET `pic4`='$new_filename4' WHERE `u_id` = '$user_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
@@ -76,7 +75,7 @@ if (move_uploaded_file($_FILES['pic_4']['tmp_name'], $target_file)) {
 
 $new_filename5 = $u_id.'_'.basename($_FILES['pic_5']['name']);
 $target_file5 = $target_dir.$new_filename5;
-if (move_uploaded_file($_FILES['pic_5']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['pic_5']['tmp_name'], $target_file5)) {
     $update_string = "UPDATE $table SET `pic5`='$new_filename5' WHERE `u_id` = '$user_id'";
     $sql = $mysqli->query($update_string);
     if ($mysqli->affected_rows > 0) {
