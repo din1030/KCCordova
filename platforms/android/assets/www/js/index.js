c2924be9ca2ff5b954787eaa95aa1a7ce7c1649f
Chia-Ting <din1030@gmail.com>
2016-09-02 02:58:32 +0800

    add .htaccess

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -327,6 +327,16 @@ $(document).on('pagecreate', '#home', function() {
 			});
 		}
 		$('#home-main').show();
+		var mask = '<div style="display:block;" class="page_mask text-center" data-position-to="window" data-dismissible="true"><a href="#" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><p>您不是店家管理者<br>求職者資訊僅供店家管理者瀏覽</p></div>';
+		$("a[href='./jobseeker.html']").click(function(event) {
+			if (window.localStorage.getItem('auth') != '0' && window.localStorage.getItem('auth') != '2') {
+				event.preventDefault();
+				$("[data-role='page']").prepend(mask);
+				$(".page_mask .ui-icon-delete").click(function(event) {
+					$(".page_mask").remove();
+				});
+			}
+		});
 	}).fail(function() {
 		alert('請確認您的網路連線狀態！');
 	});
