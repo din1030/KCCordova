8236354853d6ffd1c48fa5bda55f5caa2055dd8a
Chia-Ting <din1030@gmail.com>
2016-09-05 04:33:04 +0800

    add update policy api

---
+++ api/update_policy.php
@@ -0,0 +1,19 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+
+include 'db_setting.php';
+
+$policy = $_POST['policy'];
+$policy_content = $_POST['policy_content'];
+
+$sql_string = "UPDATE policy` SET `content`='$policy_content' WHERE `item`= '$policy'";
+if ($mysqli->query($sql_string)) {
+    echo json_encode(array('status' => true, 'message' => '已更新！'));
+
+    return;
+} else {
+    echo json_encode(array('status' => false, 'message' => '請重新操作！'));
+    exit;
+}
