059474562378a9ef370cf7d576e8e8e2e4a0811f
Chia-Ting <din1030@gmail.com>
2016-09-11 23:33:03 +0800

    bug fix

--- api/get_personal_message_new.php
+++ api/get_personal_message_new.php
@@ -23,6 +23,7 @@ if ($user_result->num_rows > 0) {
         $sql_string = 'SELECT list.*,`club_info`.`name`, `club_info`.`pic1`,`country`.`country`,`area`.`area`  FROM (SELECT `id`, MAX(`last_time`) time FROM (SELECT `messages`.`from_id` id, MAX(`time`) last_time FROM `messages` WHERE `to_id`='.$id.' GROUP BY `from_id` UNION SELECT `messages`.`to_id` id, MAX(`time`) last_time FROM `messages` WHERE `from_id`='.$id.' GROUP BY `to_id`) r GROUP BY `id` ORDER BY `time` DESC) list,`club_info`,`country`,`area` WHERE list.`id`=`club_info`.`admin_id` AND `club_info`.`country_id` = `country`.`id` AND `club_info`.`area_id` = `area`.`id`';
         $from_type = 2;
     }
+    $sql = $mysqli->query($sql_string);
     while ($r = mysqli_fetch_assoc($sql)) {
         $r['from_type'] = $from_type;
         $r['last_time'] = date('Y/m/d H:i', strtotime($r['last_time']));
