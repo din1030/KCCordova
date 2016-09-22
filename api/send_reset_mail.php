e87f69f7ef6f419b2d3800ba37057ae1d474a9d7
Chia-Ting <din1030@gmail.com>
2016-09-23 04:06:40 +0800

    bug fix

--- api/send_reset_mail.php
+++ api/send_reset_mail.php
@@ -10,7 +10,7 @@ $email = trim($_POST['reset_email']);
 $sql = $mysqli->query("SELECT * FROM `user` WHERE `email`='$email'");
 if ($sql->num_rows == 1) {
     $reset_code = rand(10000000, 99999999);
-    if ($mysql->query("UPDATE `user` SET `reset_code`='$reset_code' WHERE `email`='$email'")) {
+    if ($mysqli->query("UPDATE `user` SET `reset_code`='$reset_code' WHERE `email`='$email'")) {
         // $reset_link = 'http://52.69.53.255/KCCordova/reset_password.php?email='.$email.'&reset_code='.$reset_code;
         // $message = '請店選或複製以下連結以設定新的密碼，若您未曾申請重設密碼，忽略本信件即可。<br><a href="'.$reset_link.'">'.$reset_link.'</a>';
         $message = "您的密碼重設驗證碼為 $reset_code，若您未設申請重設密碼，忽略本信件即可。";
